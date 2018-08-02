7ae81bfcd397ebbf759c72069b6d4685e2267ed9
David Whitt <davidwhitt00@gmail.com>
2018-08-02 16:49:48 -0400

    finished nav

--- garden-tools/src/app/app.module.ts
+++ garden-tools/src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppRoutingModule } from './/app-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { MaterialModule } from './material.module';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     AboutComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
